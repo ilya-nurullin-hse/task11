4207b1886c83f41902039683d634e0d26e50d613
Ilya Nurullin <nurullinin@mail.ru>
2017-09-25 00:17:57 +0500

    Add user input

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -14,13 +14,103 @@ namespace Task11
 
         static void Main(string[] args)
         {
-            var key = GenerateKey();
+            Console.WriteLine("Шифровка текста с помощью решетки заключается в следующем. Решетка, т.е. квадрат 10х10 клеток, " +
+                              "некоторые клетки в котором вырезаны, совмещается с целым квадратом 10х10 клеток и через прорези " +
+                              "на бумагу наносятся первые буквы текста. Затем решетка поворачивается на 90 и через прорези " +
+                              "записываются следующие буквы. Это повторяется еще дважды. Таким образом, на бумагу наносится 100 " +
+                              "букв текста. Решетку можно изображать квадратной матрицей порядка 10 из нулей и единиц. Требуется " +
+                              "зашифровать данную последовательность из 100 букв, затем ее расшифровать. Даны поледовательность из " +
+                              "100 букв и матрица - ключ.Зашифровать данную последовательность.Расшифровать данную поледовательность.");
+            while (true)
+            {
+                Console.WriteLine("1. Сгенерировать матрицу-ключ автоматически");
+                Console.WriteLine("2. Задать матрицу-ключ вручную (матрица 10x10)");
+                var answer = readInt("Выберите вариант", x => x == 1 || x == 2, "Ожидалось 1 или 2");
+                int[,] key = new int[keySize, keySize];
 
-            string text = RandomString(keySize * keySize);
-            Console.WriteLine("Randrom text: {0}", text);
-            Console.WriteLine("Encoded text: {0}", Encode(key, text));
-            Console.WriteLine("Decoded text: {0}", Decode(key, Encode(key, text)));
-            Console.WriteLine("Decoded text =? Random text: {0}", Decode(key, Encode(key, text)) == text);
+                if (answer == 1)
+                {
+                    key = GenerateKey();
+                    for (int i = 0; i < keySize; i++)
+                    {
+                        for (int j = 0; j < keySize; j++)
+                        {
+                            Console.Write("{0} ", key[i, j] == -1 ? 0 : 1);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вводите матрицу построчно. Элементы в строке разделяйте пробелом. " +
+                                      "Допустимые значения элементов: 0 и 1. Количество элементов в каждой строчке = {0}",
+                        keySize);
+                    for (int i = 0; i < keySize; i++)
+                    {
+                        try
+                        {
+                            var row = Console.ReadLine().Split().Select(int.Parse).Select(x => x == 0 ? -1 : x)
+                                .ToArray();
+                            if (row.Length != keySize)
+                            {
+                                Console.WriteLine(
+                                    "Ожидалось элементов строчке: {0}, получено = {1}. Попробуйе ещё раз.",
+                                    keySize, row.Length);
+                                i--;
+                                continue;
+                            }
+                            if (row.Any(x => x != -1 && x != 1))
+                            {
+                                Console.WriteLine(
+                                    "В строке обнаружены недопустимые элементы. Разрешены только 0 и 1. Попробуйе ещё раз.");
+                                i--;
+                                continue;
+                            }
+                            for (int j = 0; j < keySize; j++)
+                            {
+                                key[i, j] = row[j];
+                            }
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Неправильный формат строки. Попробуйе ещё раз.");
+                        }
+                    }
+                    if (!CheckKey(key))
+                    {
+                        Console.WriteLine("Невалидная матрица-ключ. Попробуйте ещё раз.");
+                        continue;
+                    }
+                }
+
+                Console.WriteLine("1. Сгенерировать текст автоматически");
+                Console.WriteLine("2. Задать текст вручную (ровно 100 символов)");
+                answer = readInt("Выберите вариант", x => x == 1 || x == 2, "Ожидалось 1 или 2");
+
+                string text = "";
+
+                if (answer == 1)
+                    text = RandomString(keySize * keySize);
+                else
+                {
+                    int length = 0;
+                    do
+                    {
+                        Console.Write("Введите текст: ");
+                        text = Console.ReadLine();
+                        length = text.Length;
+                        if (length != 100)
+                        {
+                            Console.WriteLine("Текст должен быть длины 100 символов. Попробуйте ещё раз.");
+                        }
+                    } while (length != 100);
+                }
+
+                Console.WriteLine("Randrom text: {0}", text);
+                Console.WriteLine("Encoded text: {0}", Encode(key, text));
+                Console.WriteLine("Decoded text: {0}", Decode(key, Encode(key, text)));
+                Console.WriteLine("Decoded text =? Random text: {0}", Decode(key, Encode(key, text)) == text);
+            }
         }
 
         static int[,] GenerateKey()
@@ -34,8 +124,10 @@ namespace Task11
                     if (key[i, j] == 1 || key[i, j] == -1)
                         continue;
 
-                    int[][] coords = { new []{ i, j }, new[] { j, keySize - 1 - i}, new[] { keySize - 1 - j, i},
-                        new[] { keySize - 1 - i, keySize - 1 - j}
+                    int[][] coords =
+                    {
+                        new[] {i, j}, new[] {j, keySize - 1 - i}, new[] {keySize - 1 - j, i},
+                        new[] {keySize - 1 - i, keySize - 1 - j}
                     };
 
                     int r = rand.Next(0, 3);
@@ -54,7 +146,7 @@ namespace Task11
 
         static string Encode(int[,] key, string text)
         {
-            char[,] res = new char[keySize,keySize];
+            char[,] res = new char[keySize, keySize];
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < keySize; j++)
@@ -124,7 +216,7 @@ namespace Task11
 
         static int[,] RotateKey(int[,] key)
         {
-            int[,] res = new int[keySize,keySize];
+            int[,] res = new int[keySize, keySize];
             for (int i = 0; i < keySize; i++)
             {
                 for (int j = 0; j < keySize; j++)
@@ -133,6 +225,62 @@ namespace Task11
                 }
             }
             return res;
+        }
+
+        static bool CheckKey(int[,] key)
+        {
+            for (int i = 0; i < keySize; i++)
+            {
+                for (int j = 0; j < keySize; j++)
+                {
+                    if (key[i, j] == -1
+                          && ! (key[j, keySize - 1 - i] == 1
+                              || key[keySize - 1 - j, i] == 1
+                              || key[keySize - 1 - i, keySize - 1 - j] == 1)
+                    )
+                        return false;
+                    if (key[i, j] == 1 
+                    && key[j, keySize - 1 - i] != -1 
+                    && key[keySize - 1 - j, i] != -1 
+                    && key[keySize - 1 - i, keySize - 1 - j] != -1
+                    )
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        static int readInt(string msg, Func<int, bool> filter, string errFilter)
+        {
+            int n = 0;
+            bool ok = true;
+
+            do
+            {
+                try
+                {
+                    Console.Write("{0}: ", msg);
+                    n = Int32.Parse(Console.ReadLine());
+                    if (!filter(n))
+                    {
+                        Console.WriteLine(errFilter);
+                        ok = false;
+                    }
+                    else
+                        ok = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введите число");
+                    ok = false;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Вы ввели слишком большое число");
+                    ok = false;
+                }
+            } while (!ok);
+            return n;
         }
     }
 }
